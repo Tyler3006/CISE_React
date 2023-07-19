1bd3c9ad1428cc301b3a2cbb558b6d7c4ee5fb57
Tyler <tscosta01@icloud.com>
2023-07-19 13:18:03 +1200

    Updated the App.js file to include the appropriate Agile message

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aligle is about Values and Principles.
         </p>
         <a
           className="App-link"
